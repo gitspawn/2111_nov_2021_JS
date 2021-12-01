61133eb3c146fc14705632a4112737e1b6592766
gitspawn <volosgoto@gmail.com>
2021-12-01 21:47:46 +0200

    add lesson 06

---
+++ 06. arrays/sandbox/04_link_and_value.js
@@ -0,0 +1,28 @@
+// Примитивы
+// String
+// Number
+// Boolean
+
+// Ссылочные типы
+// - Oblect
+// - Array;
+
+// let a = 10;
+// let b = a + 1;
+
+// console.log(a === b);
+// console.log("a", a);
+// console.log("b", b);
+
+let a = [1, 2, 3, 4];
+let b = a;
+let c = [1, 2, 3, 4, 5];
+b.push(5);
+// a.push("Hello");
+
+console.log("a", a);
+console.log("b", b);
+console.log("c", c);
+
+// console.log(a === b);
+console.log(a === c);
