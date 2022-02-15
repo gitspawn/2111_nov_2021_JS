d8b08d144a37478059c195f6efeeee9ae2ec7fa7
gitspawn <volosgoto@gmail.com>
2022-02-15 08:58:51 +0200

    add lesson 25

---
+++ 25. Async-await, sockets, regex/sockets/app.js
@@ -0,0 +1,15 @@
+// http://localhost:3000
+// https://localhost:3000
+
+// ws://localhost:3000
+// wss://localhost:3000
+
+
+
+socket.on("message", data => {
+    console.log(data);
+  });
+
+
+
+btn.addEventListener('click', ()=>{})
